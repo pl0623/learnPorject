d0260aa660a79a67b1f80cc917b036c2aacfa798
penglei <penglei@kedacom.com>
2019-07-11 18:01:30 +0800

    cherry-pick1OC

--- demolearn/src/test2.java
+++ demolearn/src/test2.java
@@ -1,11 +1,8 @@
 public class test2 {
     public static void main(String[] args) {
         System.out.println("这时第二次添加的test2");
-<<<<<<< HEAD
-		1
-		2
-=======
-		//test checkout2
->>>>>>> parent of 5455521... Revert "git checkout 2"
+1
+
+2
     }
 }
