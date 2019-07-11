c10d236f5b510837839d6226fd2c34b10b47ef12
penglei <penglei@kedacom.com>
2019-07-11 18:16:45 +0800

    123

--- demolearn/src/test2.java
+++ demolearn/src/test2.java
@@ -1,8 +1,13 @@
 public class test2 {
     public static void main(String[] args) {
         System.out.println("这时第二次添加的test2");
+<<<<<<< HEAD
 1
 
 2
+=======
+		1
+		2
+>>>>>>> parent of 9faf812... Revert "Revert "git checkout 2""
     }
 }
